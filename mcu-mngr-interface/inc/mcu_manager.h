1205625a6cfc43fcfdbcb6e8f9f9294864bb518e
Estanislao Crivos <estanislaocrivos@gmail.com>
2025-09-09 21:25:14 -0300

    Library/mcu mngr interface (#3)
    * Updates.
    
    * Updates.
    
    * Updates.
    
    * Updates.
    
    * Updates.

---
+++ mcu-mngr-interface/inc/mcu_manager.h
@@ -0,0 +1,88 @@
+#ifndef MCU_MANAGER_H
+#define MCU_MANAGER_H
+
+/* ============================================================================================== */
+
+#include <stdint.h>
+#include <stdbool.h>
+
+/* ============================================================================================== */
+
+typedef void (*tmr_timeout_callback_t)(void);
+typedef int8_t (*tmr_set_timeout_t)(uint16_t);
+typedef void (*tmr_deact_timeout_t)(void);
+typedef void (*tmr_reset_timeout_t)(void);
+typedef int8_t (*tmr_set_timeout_callback_t)(tmr_timeout_callback_t);
+typedef void (*led_on_t)(void);
+typedef void (*led_off_t)(void);
+typedef void (*led_toggle_t)(void);
+typedef void (*gpio_high_t)(void);
+typedef void (*gpio_low_t)(void);
+typedef void (*gpio_set_state_t)(uint8_t, bool);
+typedef void (*gpio_get_state_t)(uint8_t, bool*);
+typedef void (*gpio_toggle_t)(void);
+typedef void (*disable_interrupts_t)(void);
+typedef void (*enable_interrupts_t)(void);
+
+/* ============================================================================================== */
+
+typedef struct
+{
+    disable_interrupts_t disable;
+    enable_interrupts_t  enable;
+} interrupt_ctrl_interface_t;
+
+typedef struct
+{
+    tmr_set_timeout_t          set_timeout;
+    tmr_reset_timeout_t        reset_timeout;
+    tmr_deact_timeout_t        deactivate_timeout;
+    tmr_set_timeout_callback_t set_callback;
+} timer_interface_t;
+
+typedef struct
+{
+    led_on_t     on;
+    led_off_t    off;
+    led_toggle_t toggle;
+} led_interface_t;
+
+typedef struct
+{
+    gpio_high_t      set_high;
+    gpio_low_t       set_low;
+    gpio_set_state_t set_state;
+    gpio_get_state_t get_state;
+    gpio_toggle_t    toggle;
+} gpio_interface_t;
+
+/* ============================================================================================== */
+
+typedef struct
+{
+    interrupt_ctrl_interface_t* interrupt_ctrl;
+    timer_interface_t*          timer0;
+    timer_interface_t*          timer1;
+    timer_interface_t*          timer2;
+    timer_interface_t*          timer3;
+    led_interface_t*            led_0;
+    led_interface_t*            led_1;
+    led_interface_t*            led_2;
+    gpio_interface_t*           gpio_0;
+    gpio_interface_t*           gpio_1;
+    gpio_interface_t*           gpio_2;
+    gpio_interface_t*           gpio_3;
+    gpio_interface_t*           gpio_4;
+    gpio_interface_t*           gpio_5;
+    gpio_interface_t*           gpio_6;
+    gpio_interface_t*           gpio_7;
+    gpio_interface_t*           gpio_8;
+    gpio_interface_t*           gpio_9;
+    gpio_interface_t*           gpio_10;
+    gpio_interface_t*           gpio_11;
+    gpio_interface_t*           gpio_12;
+} mcu_manager_interface_t;
+
+/* ============================================================================================== */
+
+#endif
---
+++ mcu-mngr-interface/test/test_mcu_manager.c
@@ -0,0 +1,57 @@
+#include "../../test/support/unity.h"
+
+/* ============================================================================================== */
+
+#include "../inc/mcu_manager.h"
+#include "../src/mcu_manager.c"
+
+/* Mocked interfaces */
+#include "mock_mcu_driver.h"
+
+/* ============================================================================================== */
+
+static interrupt_ctrl_interface_t fake_interrupt_ctrl;
+static timer_interface_t          fake_timer0;
+static led_interface_t            fake_led_0;
+static gpio_interface_t           fake_gpio_0;
+
+void helper_mcu_mngr_init(mcu_manager_interface_t* mcu_mngr)
+{
+    mcu_mngr->interrupt_ctrl = &fake_interrupt_ctrl;
+    mcu_mngr->timer0         = &fake_timer0;
+    mcu_mngr->led_0          = &fake_led_0;
+    mcu_mngr->gpio_0         = &fake_gpio_0;
+
+    mcu_mngr->interrupt_ctrl->disable    = mcu_mngr_disable_interrupts;
+    mcu_mngr->interrupt_ctrl->enable     = mcu_mngr_enable_interrupts;
+    mcu_mngr->timer0->set_timeout        = mcu_mngr_tmr0_set_timeout_ms;
+    mcu_mngr->timer0->reset_timeout      = mcu_mngr_tmr0_reset_timeout;
+    mcu_mngr->timer0->deactivate_timeout = mcu_mngr_tmr0_deactivate_timeout;
+    mcu_mngr->timer0->set_callback       = mcu_mngr_tmr0_set_timeout_callback;
+    mcu_mngr->led_0->on                  = mcu_mngr_led_turn_on;
+    mcu_mngr->led_0->off                 = mcu_mngr_led_turn_off;
+    mcu_mngr->led_0->toggle              = mcu_mngr_led_toggle;
+    mcu_mngr->gpio_0->set_state          = mcu_mngr_set_pin_state;
+    mcu_mngr->gpio_0->get_state          = mcu_mngr_get_pin_state;
+}
+
+/* ============================================================================================== */
+
+void test_mcu_mngr_creation(void)
+{
+    mcu_manager_interface_t mcu_mngr = {0};
+    helper_mcu_mngr_init(&mcu_mngr);
+    TEST_ASSERT_EQUAL_PTR(mcu_mngr_disable_interrupts, mcu_mngr.interrupt_ctrl->disable);
+    TEST_ASSERT_EQUAL_PTR(mcu_mngr_enable_interrupts, mcu_mngr.interrupt_ctrl->enable);
+    TEST_ASSERT_EQUAL_PTR(mcu_mngr_tmr0_set_timeout_ms, mcu_mngr.timer0->set_timeout);
+    TEST_ASSERT_EQUAL_PTR(mcu_mngr_tmr0_reset_timeout, mcu_mngr.timer0->reset_timeout);
+    TEST_ASSERT_EQUAL_PTR(mcu_mngr_tmr0_deactivate_timeout, mcu_mngr.timer0->deactivate_timeout);
+    TEST_ASSERT_EQUAL_PTR(mcu_mngr_tmr0_set_timeout_callback, mcu_mngr.timer0->set_callback);
+    TEST_ASSERT_EQUAL_PTR(mcu_mngr_led_turn_on, mcu_mngr.led_0->on);
+    TEST_ASSERT_EQUAL_PTR(mcu_mngr_led_turn_off, mcu_mngr.led_0->off);
+    TEST_ASSERT_EQUAL_PTR(mcu_mngr_led_toggle, mcu_mngr.led_0->toggle);
+    TEST_ASSERT_EQUAL_PTR(mcu_mngr_set_pin_state, mcu_mngr.gpio_0->set_state);
+    TEST_ASSERT_EQUAL_PTR(mcu_mngr_get_pin_state, mcu_mngr.gpio_0->get_state);
+}
+
+/* ============================================================================================== */
