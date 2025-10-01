13941595fdce600836d9691e6368446a2322f8f9
estanislaocrivos <estanislaocrivos@gmail.com>
2025-09-30 22:36:08 -0300

    Updates.

--- inc/c_libraries.h
+++ inc/c_libraries.h
@@ -6,6 +6,7 @@
 #include "errno.h"
 #include "../lcd-16-by-2/inc/lcd.h"
 #include "../embedded-hal/inc/embedded_hal.h"
+#include "../esp8266ex-wifi-module/inc/esp8266ex.h"
 
 /* ============================================================================================== */
 
