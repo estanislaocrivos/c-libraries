8cb926bd1aa3797b1f0444de4f505fbdd179e2e8
estanislaocrivos <estanislaocrivos@gmail.com>
2025-10-05 18:46:27 -0300

    Updates.

--- drivers/atmega2560/inc/config.h
+++ drivers/atmega2560/inc/config.h
@@ -3,6 +3,13 @@
 
 #define F_CPU 16000000UL
 
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+#include "../../c-libraries/inc/errno.h"
+#include "../../c-libraries/inc/c_libraries.h"
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
