e136b15bf3a598040bc06e2129f1bf8d29429f78
Estanislao Crivos <estanislaocrivos@gmail.com>
2025-10-15 14:38:36 -0300

    Updates. (#20)

--- libraries/embedded-hal/inc/interrupt.h
+++ libraries/embedded-hal/inc/interrupt.h
@@ -17,6 +17,17 @@ struct interrupt_ops
      * @brief Enables global interrupts.
      */
     void (*enable_global)(bool);
+
+    /**
+     * @brief Makes a code section atomic by disabling interrupts. Must allow multiple nested calls
+     * with internal counter.
+     */
+    void (*atomic_lock)(void);
+
+    /**
+     * @brief Restores the previous interrupt state. Must match a previous call to atomic_lock().
+     */
+    void (*atomic_unlock)(void);
 };
 
 /* ============================================================================================== */
