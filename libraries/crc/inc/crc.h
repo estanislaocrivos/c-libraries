900984bdbf1fc325f42064e9836b37d87e218b58
estanislaocrivos <estanislaocrivos@gmail.com>
2025-10-21 17:22:01 -0300

    Updates.

--- libraries/crc/inc/crc.h
+++ libraries/crc/inc/crc.h
@@ -3,4 +3,48 @@
 
 /* ============================================================================================== */
 
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+
+/* ============================================================================================== */
+
+#include "../../../inc/errno.h"
+
+/* ============================================================================================== */
+
+struct crc
+{
+    /**
+     * @brief CRC-8 polynomial.
+     */
+    uint8_t crc8_polynomial;
+
+    /**
+     * @brief CRC-8 initial value.
+     */
+    uint8_t crc8_initial_value;
+
+    /**
+     * @brief CRC-8 final XOR value.
+     */
+    uint8_t crc8_final_xor_value;
+
+    /**
+     * @brief Reflect input bytes.
+     */
+    bool reflect_input;
+
+    /**
+     * @brief Reflect output bytes.
+     */
+    bool reflect_output;
+};
+
+/* ============================================================================================== */
+
+int8_t crc8_calculate(const struct crc* crc, const uint8_t* data, size_t length, uint8_t* result);
+
+/* ============================================================================================== */
+
 #endif /* CRC_H */
--- libraries/crc/test/test_crc.c
+++ libraries/crc/test/test_crc.c
@@ -8,9 +8,31 @@ TEST_SOURCE_FILE("../src/crc.c")
 
 /* ============================================================================================== */
 
-void test_example(void)
+void test_crc8_autosar(void)
 {
-    TEST_ASSERT_TRUE(1);
+    struct crc crc8_autosar = {.crc8_final_xor_value = 0xFF,
+                               .crc8_initial_value   = 0xFF,
+                               .crc8_polynomial      = 0x2F,
+                               .reflect_input        = false,
+                               .reflect_output       = false};
+
+    const uint8_t data_1[] = {0xC2};
+    uint8_t       result   = 0;
+    int8_t        status   = crc8_calculate(&crc8_autosar, data_1, sizeof(data_1), &result);
+    TEST_ASSERT_EQUAL_INT8(0, status);
+    TEST_ASSERT_EQUAL_UINT8(0xE6, result);
+
+    const uint8_t data_2[] = {0xAA, 0xBB, 0xCC};
+    result                 = 0;
+    status                 = crc8_calculate(&crc8_autosar, data_2, sizeof(data_2), &result);
+    TEST_ASSERT_EQUAL_INT8(0, status);
+    TEST_ASSERT_EQUAL_UINT8(0xCF, result);
+
+    const uint8_t data_3[] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
+    result                 = 0;
+    status                 = crc8_calculate(&crc8_autosar, data_3, sizeof(data_3), &result);
+    TEST_ASSERT_EQUAL_INT8(0, status);
+    TEST_ASSERT_EQUAL_UINT8(0x88, result);
 }
 
 /* ============================================================================================== */
