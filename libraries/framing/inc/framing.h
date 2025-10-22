0ec34a15e53cdd54da4dc738aabb2b7c143a8722
estanislaocrivos <estanislaocrivos@gmail.com>
2025-10-22 13:45:38 -0300

    Updates.

--- libraries/framing/inc/framing.h
+++ libraries/framing/inc/framing.h
@@ -17,41 +17,49 @@
  */
 enum framing_state
 {
-    FRAMING_STATE_IDLE,
-    FRAMING_STATE_START,
-    FRAMING_STATE_LENGTH,
-    FRAMING_STATE_PAYLOAD,
-    FRAMING_STATE_CRC,
-    FRAMING_STATE_STOP,
-    FRAMING_STATE_COMPLETE,
-    FRAMING_STATE_ERROR
+    FRAMING_IDLE_STATE = 0,
+    FRAMING_START_STATE,
+    FRAMING_LENGTH_STATE,
+    FRAMING_PAYLOAD_STATE,
+    FRAMING_STOP_STATE,
+    FRAMING_CRC_STATE,
+    FRAMING_COMPLETE_STATE,
+    FRAMING_ERROR_STATE
 };
 
 struct framing
 {
+    // Public
     struct crc*         crc_calculator;
-    struct ring_buffer* rx_ring_buffer;
-    uint8_t*            rx_payload_buffer;
-    uint8_t*            tx_frame_buffer;
+    struct ring_buffer* rx_raw_buffer;
+    struct ring_buffer* rx_payload_buffer;
+    struct ring_buffer* tx_frame_buffer;
     uint8_t             start_delimiter;
     uint8_t             stop_delimiter;
     bool                use_crc8;
     bool                use_crc16;
+
+    // Private
+    size_t                  _payload_size;
+    uint8_t*                _buffer;
+    size_t                  _buffer_size;
+    size_t                  _buffer_index;
+    enum framing_state      _current_state;
+    framing_state_handler_t _state_handler;
+    bool                    _was_initialized;
 };
 
 /* ============================================================================================== */
 
-int8_t framing_init(struct framing* framing_instance);
+int8_t framing_init(struct framing* self);
 
 /* ============================================================================================== */
 
-int8_t build_frame(struct framing* framing_instance, const uint8_t* payload, size_t payload_size);
+int8_t build_frame(struct framing* self, const uint8_t* payload, size_t payload_size);
 
 /* ============================================================================================== */
 
-int8_t retrieve_payload(struct framing* framing_instance,
-                        uint8_t*        payload_buffer,
-                        size_t*         payload_size);
+int8_t retrieve_payload(struct framing* self, uint8_t* payload, size_t payload_size);
 
 /* ============================================================================================== */
 
--- libraries/framing/src/framing.c
+++ libraries/framing/src/framing.c
@@ -1,3 +1,112 @@
-#include "../inc/library.h"
+#include "../inc/framing.h"
+
+/* ============================================================================================== */
+
+/* RX state machine implementation */
+
+/* ============================================================================================== */
+
+static void _fill_internal_buffer(struct framing* self, uint8_t byte)
+{
+    if (self->_buffer_index < self->_buffer_size)
+    {
+        self->_buffer[self->_buffer_index] = byte;
+        self->_buffer_index += 1;
+    }
+}
+
+typedef enum framing_state (*framing_state_handler_t)(struct framing* self, uint8_t byte);
+
+typedef struct
+{
+    framing_state_handler_t handler;
+} framing_state_table_entry_t;
+
+static enum framing_state start_state_handler(struct framing* self, uint8_t byte);
+static enum framing_state length_state_handler(struct framing* self, uint8_t byte);
+static enum framing_state payload_state_handler(struct framing* self, uint8_t byte);
+static enum framing_state stop_state_handler(struct framing* self, uint8_t byte);
+static enum framing_state crc_state_handler(struct framing* self, uint8_t byte);
+static enum framing_state complete_state_handler(struct framing* self, uint8_t byte);
+static enum framing_state error_state_handler(struct framing* self, uint8_t byte);
+
+/* State table. State handlers are in order according to the enum framing_state enum */
+static const framing_state_table_entry_t state_table[] = {
+    {start_state_handler}, {length_state_handler},   {payload_state_handler}, {stop_state_handler},
+    {crc_state_handler},   {complete_state_handler}, {error_state_handler},
+};
+
+static enum framing_state start_state_handler(struct framing* self, uint8_t byte)
+{
+    if (byte != self->start_delimiter)
+    {
+        return FRAMING_ERROR_STATE;
+    }
+    _fill_internal_buffer(self, byte);
+    return FRAMING_LENGTH_STATE;
+}
+
+static enum framing_state length_state_handler(struct framing* self, uint8_t byte)
+{
+    self->_payload_size = (size_t)byte;
+    _fill_internal_buffer(self, byte);
+    return FRAMING_PAYLOAD_STATE;
+}
+
+static enum framing_state payload_state_handler(struct framing* self, uint8_t byte)
+{
+    _fill_internal_buffer(self, byte);
+    if (self->_buffer_index == self->_payload_size + 1)
+    {
+        /* If payload is full, expect ETX */
+        return FRAMING_STOP_STATE;
+    }
+    return FRAMING_PAYLOAD_STATE;
+}
+
+static enum framing_state stop_state_handler(struct framing* self, uint8_t byte)
+{
+    if (byte != self->stop_delimiter)
+    {
+        return FRAMING_ERROR_STATE;
+    }
+    _fill_internal_buffer(self, byte);
+    return FRAMING_CRC_STATE;
+}
+
+static enum framing_state crc_state_handler(struct framing* self, uint8_t byte)
+{
+    uint8_t calculated_crc;
+    crc8_calculate(self->crc_calculator, self->_buffer, self->_buffer_index, &calculated_crc);
+    if (byte == calculated_crc)
+    {
+        return FRAMING_COMPLETE_STATE;
+    }
+    return FRAMING_ERROR_STATE;
+}
+
+static enum framing_state error_state_handler(struct framing* self, uint8_t byte)
+{
+    return FRAMING_ERROR_STATE;
+}
+
+static enum framing_state complete_state_handler(struct framing* self, uint8_t byte)
+{
+    return FRAMING_COMPLETE_STATE;
+}
+
+/* ============================================================================================== */
+
+int8_t retrieve_payload(struct framing* self, uint8_t* payload, size_t payload_size)
+{
+    if (self == NULL || payload == NULL || payload_size == NULL)
+    {
+        return -EFAULT;
+    }
+    if (pop(self->rx_ring_buffer, payload, payload_size))
+    {
+        return -EIO;
+    }
+}
 
 /* ============================================================================================== */
