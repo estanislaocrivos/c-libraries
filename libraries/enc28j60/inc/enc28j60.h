4a796c08a32e79ef89f504f53a53d1768a25bd16
estanislaocrivos <estanislaocrivos@gmail.com>
2026-06-06 15:43:08 -0300

    feat(enc28j60): implement ENC28J60 driver with updated CMake configuration and header files

--- libraries/enc28j60/inc/enc28j60.h
+++ libraries/enc28j60/inc/enc28j60.h
@@ -1,14 +1,16 @@
 #ifndef ENC28J60_H
 #define ENC28J60_H
 
-#include "spi.h"
+#include "../../embedded-hal/inc/spi.h"
 
 #include <stdint.h>
 
 struct enc28j60
 {
-    uint8_t    id;
     struct spi spi_bus;
-}
+};
+
+int8_t read_raw_register(
+    const struct enc28j60* self, uint8_t address, uint8_t* value);
 
 #endif /* ENC28J60_H */
--- libraries/enc28j60/src/enc28j60.c
+++ libraries/enc28j60/src/enc28j60.c
@@ -1,3 +1,20 @@
-#include "enc28j60.h"
+#include "../inc/enc28j60.h"
+
+#include "../../inc/errno.h"
+
+#include <stdint.h>
+
+static uint8_t tx_buffer[32];
+static uint8_t rx_buffer[32];
+
+int8_t read_raw_register(
+    const struct enc28j60* self, uint8_t address, uint8_t* value)
+{
+    if (self->spi_bus.ops->transfer(&self->spi_bus, tx_buffer, rx_buffer, 32))
+    {
+        return -EIO;
+    }
+    return 0;
+}
 
 /* ========================================================================== */
