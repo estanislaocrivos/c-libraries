dbef71782bdd3767e5ac271099a5747e41161e16
Estanislao Crivos <estanislaocrivos@gmail.com>
2026-06-11 21:44:46 -0300

    feat(enc28j60): add ENC28J60 memory bank definitions and update register functions

--- libraries/enc28j60/inc/enc28j60.h
+++ libraries/enc28j60/inc/enc28j60.h
@@ -1,18 +1,48 @@
 #ifndef ENC28J60_H
 #define ENC28J60_H
 
+#include "../../embedded-hal/inc/gpio.h"
 #include "../../embedded-hal/inc/spi.h"
 
 #include <stdint.h>
 
 /* ========================================================================== */
 
+enum ENC28J60_MEM_BANK
+{
+    BANK_0  = 0x00,
+    BANK_1  = 0x01,
+    BANK_2  = 0x02,
+    BANK_3  = 0x03,
+    NO_BANK = 0xFF,
+};
+
+typedef uint16_t enc28j60_reg_t;
+
+#define EREVID ((enc28j60_reg_t)(BANK_3 << 8) | 0x12)
+#define EIE    ((enc28j60_reg_t)(NO_BANK << 8) | 0x1B)
+#define EIR    ((enc28j60_reg_t)(NO_BANK << 8) | 0x1C)
+#define ESTAT  ((enc28j60_reg_t)(NO_BANK << 8) | 0x1D)
+#define ECON2  ((enc28j60_reg_t)(NO_BANK << 8) | 0x1E)
+#define ECON1  ((enc28j60_reg_t)(NO_BANK << 8) | 0x1F)
+
+/* ========================================================================== */
+
 struct enc28j60
 {
-    struct spi spi_bus;
+    const struct gpio* const spi_cs;
+    const struct spi* const  spi_bus;
 };
 
 /* ========================================================================== */
+
+int8_t enc28j60_init(const struct enc28j60* self);
+
+int8_t enc28j60_write_register(
+    const struct enc28j60* self, uint16_t address, uint8_t value);
+
+int8_t enc28j60_read_register(
+    const struct enc28j60* self, uint16_t address, uint8_t* value);
 
 /* ========================================================================== */
 
--- libraries/enc28j60/src/enc28j60.c
+++ libraries/enc28j60/src/enc28j60.c
@@ -2,18 +2,10 @@
 
 #include "../../inc/errno.h"
 
+#include <stdbool.h>
 #include <stdint.h>
 
 /* ========================================================================== */
-
-enum ENC28J60_MEM_BANK
-{
-    BANK_0  = 0x00,
-    BANK_1  = 0x01,
-    BANK_2  = 0x02,
-    BANK_3  = 0x03,
-    NO_BANK = 0xFF,
-};
 
 enum ENC28J60_INSTRUC_OP_CODE
 {
@@ -25,16 +17,6 @@ enum ENC28J60_INSTRUC_OP_CODE
     BIT_FIELD_CLEAR = 0x05,
     SYSTEM_RESET    = 0x07,
 };
-
-/* ========================================================================== */
-
-typedef uint16_t enc28j60_reg_t;
-
-#define EIE   ((enc28j60_reg_t)(NO_BANK << 8) | 0x1B)
-#define EIR   ((enc28j60_reg_t)(NO_BANK << 8) | 0x1C)
-#define ESTAT ((enc28j60_reg_t)(NO_BANK << 8) | 0x1D)
-#define ECON2 ((enc28j60_reg_t)(NO_BANK << 8) | 0x1E)
-#define ECON1 ((enc28j60_reg_t)(NO_BANK << 8) | 0x1F)
 
 /* ========================================================================== */
 
@@ -58,20 +40,28 @@ static int8_t enc28j60_select_bank(
     uint8_t tx_payload[2];
     tx_payload[0] = (BIT_FIELD_CLEAR << 5) | (uint8_t)ECON1;
     tx_payload[1] = 0x03; /* Clear only last to bits (the chip does ~& op.) */
-    if (self->spi_bus.ops->transmit(
-            &self->spi_bus, tx_payload, sizeof(tx_payload)))
+
+    self->spi_cs->ops->set_state(self->spi_cs, false);
+    if (self->spi_bus->ops->transmit(
+            self->spi_bus, tx_payload, sizeof(tx_payload)))
     {
+        self->spi_cs->ops->set_state(self->spi_cs, true);
         return -EIO;
     }
+    self->spi_cs->ops->set_state(self->spi_cs, true);
 
     /* Now write the last two bits of ECON1 */
     tx_payload[0] = ((BIT_FIELD_SET << 5) | (uint8_t)ECON1);
     tx_payload[1] = (bank & 0x03); /* Only allow 0-3 values */
-    if (self->spi_bus.ops->transmit(
-            &self->spi_bus, tx_payload, sizeof(tx_payload)))
+
+    self->spi_cs->ops->set_state(self->spi_cs, false);
+    if (self->spi_bus->ops->transmit(
+            self->spi_bus, tx_payload, sizeof(tx_payload)))
     {
+        self->spi_cs->ops->set_state(self->spi_cs, true);
         return -EIO;
     }
+    self->spi_cs->ops->set_state(self->spi_cs, true);
 
     current_bank = bank;
     return 0;
@@ -79,39 +69,57 @@ static int8_t enc28j60_select_bank(
 
 /* ========================================================================== */
 
-int8_t write_register(
+int8_t enc28j60_write_register(
     const struct enc28j60* self, uint16_t address, uint8_t value)
 {
     uint8_t bank = (uint8_t)(address >> 8);
     enc28j60_select_bank(self, bank);
 
-    uint8_t tx_payload[2] = {(WRITE_CTRL_REG << 5) | address, value};
-    if (self->spi_bus.ops->transmit(
-            &self->spi_bus, tx_payload, sizeof(tx_payload)))
+    uint8_t tx_payload[2] = {(uint8_t)((WRITE_CTRL_REG << 5) | address), value};
+    self->spi_cs->ops->set_state(self->spi_cs, false);
+    if (self->spi_bus->ops->transmit(
+            self->spi_bus, tx_payload, sizeof(tx_payload)))
     {
+        self->spi_cs->ops->set_state(self->spi_cs, true);
         return -EIO;
     }
+    self->spi_cs->ops->set_state(self->spi_cs, true);
     return 0;
 }
 
 /* ========================================================================== */
 
-int8_t read_register(
+int8_t enc28j60_read_register(
     const struct enc28j60* self, uint16_t address, uint8_t* value)
 {
     uint8_t bank = (uint8_t)(address >> 8);
     enc28j60_select_bank(self, bank);
 
-    uint8_t tx_payload[] = {(READ_CTRL_REG << 5) | address, 0x00};
+    uint8_t tx_payload[] = {(uint8_t)((READ_CTRL_REG << 5) | address), 0x00};
     uint8_t rx_payload[2];
 
-    if (self->spi_bus.ops->transfer(
-            &self->spi_bus, tx_payload, rx_payload, sizeof(rx_payload)))
+    self->spi_cs->ops->set_state(self->spi_cs, false);
+    if (self->spi_bus->ops->transfer(
+            self->spi_bus, tx_payload, rx_payload, sizeof(rx_payload)))
     {
+        self->spi_cs->ops->set_state(self->spi_cs, true);
         return -EIO;
     }
+    self->spi_cs->ops->set_state(self->spi_cs, true);
     *value = rx_payload[1];
     return 0;
 }
 
 /* ========================================================================== */
+
+int8_t enc28j60_init(const struct enc28j60* self)
+{
+    if (self == NULL)
+    {
+        return -EFAULT;
+    }
+    if (self->spi_bus == NULL || self->spi_cs == NULL)
+    {
+        return -EFAULT;
+    }
+}
