e04a2397c81630934d85696ed569b2cdf728e296
Estanislao Crivos <estanislaocrivos@gmail.com>
2026-06-28 22:53:50 -0300

    refactor(arp): use metadata structs for frame handling

--- libraries/pico-net-stack/inc/arp.h
+++ libraries/pico-net-stack/inc/arp.h
@@ -8,16 +8,46 @@
 
 /* ========================================================================== */
 
+enum arp_op_type
+{
+    ARP_REQUEST,
+    ARP_REPLY,
+    ARP_REV_REQUEST,
+    ARP_REV_REPLY,
+    ARP_UNKNOWN_TYPE,
+};
+
 struct arp
 {
     uint8_t ip_addr[4];
     uint8_t mac_addr[6];
 };
 
-int8_t arp_is_request_for_me(
-    const struct arp* self, uint8_t* frame, uint8_t size, bool* state);
+struct arp_rx_metadata
+{
+    enum arp_op_type op_type;
+    uint8_t          src_ip_addr[4];
+    uint8_t          src_mac_addr[6];
+};
 
-int8_t arp_reply_request(const struct arp* self, uint8_t* frame, uint8_t size);
+struct arp_tx_metadata
+{
+    enum arp_op_type op_type;
+    uint8_t          dest_ip_addr[4];
+    uint8_t          dest_mac_addr[6];
+};
+
+int8_t arp_process_frame(
+    const struct arp*       self,
+    const uint8_t*          rx_frame,
+    uint8_t                 rx_frame_size,
+    struct arp_rx_metadata* mdata);
+
+int8_t arp_build_frame(
+    const struct arp*       self,
+    struct arp_tx_metadata* mdata,
+    uint8_t*                tx_frame,
+    uint8_t                 tx_frame_size);
 
 /* ========================================================================== */
 
--- libraries/pico-net-stack/src/arp.c
+++ libraries/pico-net-stack/src/arp.c
@@ -4,121 +4,150 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
 
 /* ========================================================================== */
 
-#define MIN_ARP_FRAME_SIZE            42
-#define ETH_TYPE_ARP_REQUEST_OR_REPLY 0x0806
-#define OP_TYPE_ARP_REQUEST           0x01
+#define MIN_ARP_FRAME_SIZE         42
 
-#define DEST_MAC_ADDR_FRAME_OFST      0
-#define SOURCE_MAC_ADDR_FRAME_OFST    6
-#define ETH_TYPE_FRAME_OFST           12
-#define HARD_TYPE_FRAME_OFST          14
-#define PROT_TYPE_FRAME_OFST          16
-#define HARD_SIZE_FRAME_OFST          18
-#define PROT_SIZE_FRAME_OFST          19
-#define OP_FRAME_OFST                 20
-#define SENDER_MAC_ADDR_FRAME_OFST    22
-#define SENDER_IP_ADDR_FRAME_OFST     28
-#define TARGET_MAC_ADDR_FRAME_OFST    32
-#define TARGET_IP_ADDR_FRAME_OFST     38
+#define ARP_REQUEST_OPCODE_VAL     ((uint16_t)0x0001)
+#define ARP_REPLY_OPCODE_VAL       ((uint16_t)0x0002)
+#define ARP_REV_REQUEST_OPCODE_VAL ((uint16_t)0x0003)
+#define ARP_REV_REPLY_OPCODE_VAL   ((uint16_t)0x0004)
+
+#define HARD_TYPE_VAL              ((uint16_t)0x0001)
+#define PROT_TYPE_VAL              ((uint16_t)0x0800)
+#define HARD_SIZE_VAL              6
+#define PROT_SIZE_VAL              4
+
+#define HARD_TYPE_FRAME_OFST       14
+#define PROT_TYPE_FRAME_OFST       16
+#define HARD_SIZE_FRAME_OFST       18
+#define PROT_SIZE_FRAME_OFST       19
+#define OPCODE_FRAME_OFST          20
+#define SRC_MAC_ADDR_FRAME_OFST    22
+#define SRC_IP_ADDR_FRAME_OFST     28
+#define DEST_MAC_ADDR_FRAME_OFST   32
+#define DEST_IP_ADDR_FRAME_OFST    38
 
 /* ========================================================================== */
 
-int8_t arp_is_request_for_me(
-    const struct arp* self, uint8_t* frame, uint8_t size, bool* state)
+int8_t arp_process_frame(
+    const struct arp*       self,
+    const uint8_t*          rx_frame,
+    uint8_t                 rx_frame_size,
+    struct arp_rx_metadata* mdata)
+
 {
-    if (self == NULL || frame == NULL)
+    if (self == NULL || rx_frame == NULL)
     {
         return -EFAULT;
     }
-    if (size < MIN_ARP_FRAME_SIZE)
+    if (rx_frame_size < MIN_ARP_FRAME_SIZE)
     {
         return -EINVAL;
     }
 
-    uint16_t eth_type = ((uint16_t)frame[ETH_TYPE_FRAME_OFST] << 8)
-                        | (uint16_t)frame[ETH_TYPE_FRAME_OFST + 1];
-    if (eth_type != ETH_TYPE_ARP_REQUEST_OR_REPLY)
+    uint16_t op_type = ((uint16_t)rx_frame[OPCODE_FRAME_OFST] << 8)
+                       | (uint16_t)rx_frame[OPCODE_FRAME_OFST + 1];
+    switch (op_type)
     {
-        return -EINVAL;
+        case ARP_REQUEST_OPCODE_VAL:
+        {
+            mdata->op_type = ARP_REQUEST;
+            break;
+        }
+        case ARP_REPLY_OPCODE_VAL:
+        {
+            mdata->op_type = ARP_REPLY;
+            break;
+        }
+        case ARP_REV_REQUEST_OPCODE_VAL:
+        {
+            mdata->op_type = ARP_REV_REQUEST;
+            break;
+        }
+        case ARP_REV_REPLY_OPCODE_VAL:
+        {
+            mdata->op_type = ARP_REV_REPLY;
+            break;
+        }
+        default:
+        {
+            return -EINVAL;
+        }
     }
 
-    uint16_t op_type = ((uint16_t)frame[OP_FRAME_OFST] << 8)
-                       | (uint16_t)frame[OP_FRAME_OFST + 1];
-    if (op_type != OP_TYPE_ARP_REQUEST)
-    {
-        return -EINVAL;
-    }
-
-    *state
-        = (frame[TARGET_IP_ADDR_FRAME_OFST] == self->ip_addr[0]
-           && frame[TARGET_IP_ADDR_FRAME_OFST + 1] == self->ip_addr[1]
-           && frame[TARGET_IP_ADDR_FRAME_OFST + 2] == self->ip_addr[2]
-           && frame[TARGET_IP_ADDR_FRAME_OFST + 3] == self->ip_addr[3]);
+    memcpy(mdata->src_ip_addr, rx_frame + SRC_IP_ADDR_FRAME_OFST, 4);
+    memcpy(mdata->src_mac_addr, rx_frame + SRC_MAC_ADDR_FRAME_OFST, 6);
     return 0;
 }
 
 /* ========================================================================== */
 
-int8_t arp_reply_request(const struct arp* self, uint8_t* frame, uint8_t size)
+int8_t arp_build_frame(
+    const struct arp*       self,
+    struct arp_tx_metadata* mdata,
+    uint8_t*                tx_frame,
+    uint8_t                 tx_frame_size)
 {
-    if (self == NULL || frame == NULL)
+    if (self == NULL || mdata == NULL || tx_frame == NULL)
     {
         return -EFAULT;
     }
-    if (size < MIN_ARP_FRAME_SIZE)
+    if (tx_frame_size < MIN_ARP_FRAME_SIZE)
     {
         return -EINVAL;
     }
 
-    frame[DEST_MAC_ADDR_FRAME_OFST]     = frame[SENDER_MAC_ADDR_FRAME_OFST];
-    frame[DEST_MAC_ADDR_FRAME_OFST + 1] = frame[SENDER_MAC_ADDR_FRAME_OFST + 1];
-    frame[DEST_MAC_ADDR_FRAME_OFST + 2] = frame[SENDER_MAC_ADDR_FRAME_OFST + 2];
-    frame[DEST_MAC_ADDR_FRAME_OFST + 3] = frame[SENDER_MAC_ADDR_FRAME_OFST + 3];
-    frame[DEST_MAC_ADDR_FRAME_OFST + 4] = frame[SENDER_MAC_ADDR_FRAME_OFST + 4];
-    frame[DEST_MAC_ADDR_FRAME_OFST + 5] = frame[SENDER_MAC_ADDR_FRAME_OFST + 5];
+    tx_frame[HARD_TYPE_FRAME_OFST]     = (uint8_t)(HARD_TYPE_VAL >> 8);
+    tx_frame[HARD_TYPE_FRAME_OFST + 1] = (uint8_t)(HARD_TYPE_VAL);
+    tx_frame[PROT_TYPE_FRAME_OFST]     = (uint8_t)(PROT_TYPE_VAL >> 8);
+    tx_frame[PROT_TYPE_FRAME_OFST + 1] = (uint8_t)(PROT_TYPE_VAL);
+    tx_frame[HARD_SIZE_FRAME_OFST]     = (uint8_t)(HARD_SIZE_VAL);
+    tx_frame[PROT_SIZE_FRAME_OFST]     = (uint8_t)(PROT_SIZE_VAL);
 
-    frame[SOURCE_MAC_ADDR_FRAME_OFST]     = self->mac_addr[0];
-    frame[SOURCE_MAC_ADDR_FRAME_OFST + 1] = self->mac_addr[1];
-    frame[SOURCE_MAC_ADDR_FRAME_OFST + 2] = self->mac_addr[2];
-    frame[SOURCE_MAC_ADDR_FRAME_OFST + 3] = self->mac_addr[3];
-    frame[SOURCE_MAC_ADDR_FRAME_OFST + 4] = self->mac_addr[4];
-    frame[SOURCE_MAC_ADDR_FRAME_OFST + 5] = self->mac_addr[5];
+    switch (mdata->op_type)
+    {
+        case ARP_REQUEST:
+        {
+            tx_frame[OPCODE_FRAME_OFST]
+                = (uint8_t)(ARP_REQUEST_OPCODE_VAL >> 8);
+            tx_frame[OPCODE_FRAME_OFST + 1] = (uint8_t)(ARP_REQUEST_OPCODE_VAL);
+            break;
+        }
+        case ARP_REPLY:
+        {
+            tx_frame[OPCODE_FRAME_OFST] = (uint8_t)(ARP_REPLY_OPCODE_VAL >> 8);
+            tx_frame[OPCODE_FRAME_OFST + 1] = (uint8_t)(ARP_REPLY_OPCODE_VAL);
+            break;
+        }
+        case ARP_REV_REQUEST:
+        {
+            tx_frame[OPCODE_FRAME_OFST]
+                = (uint8_t)(ARP_REV_REQUEST_OPCODE_VAL >> 8);
+            tx_frame[OPCODE_FRAME_OFST + 1]
+                = (uint8_t)(ARP_REV_REQUEST_OPCODE_VAL);
+            break;
+        }
+        case ARP_REV_REPLY:
+        {
+            tx_frame[OPCODE_FRAME_OFST]
+                = (uint8_t)(ARP_REV_REPLY_OPCODE_VAL >> 8);
+            tx_frame[OPCODE_FRAME_OFST + 1]
+                = (uint8_t)(ARP_REV_REPLY_OPCODE_VAL);
+            break;
+        }
+        default:
+        {
+            return -EINVAL;
+        }
+    }
 
-    frame[OP_FRAME_OFST]     = 0;
-    frame[OP_FRAME_OFST + 1] = 2;
-
-    frame[TARGET_MAC_ADDR_FRAME_OFST]     = frame[DEST_MAC_ADDR_FRAME_OFST];
-    frame[TARGET_MAC_ADDR_FRAME_OFST + 1] = frame[DEST_MAC_ADDR_FRAME_OFST + 1];
-    frame[TARGET_MAC_ADDR_FRAME_OFST + 2] = frame[DEST_MAC_ADDR_FRAME_OFST + 2];
-    frame[TARGET_MAC_ADDR_FRAME_OFST + 3] = frame[DEST_MAC_ADDR_FRAME_OFST + 3];
-    frame[TARGET_MAC_ADDR_FRAME_OFST + 4] = frame[DEST_MAC_ADDR_FRAME_OFST + 4];
-    frame[TARGET_MAC_ADDR_FRAME_OFST + 5] = frame[DEST_MAC_ADDR_FRAME_OFST + 5];
-
-    frame[SENDER_MAC_ADDR_FRAME_OFST] = frame[SOURCE_MAC_ADDR_FRAME_OFST];
-    frame[SENDER_MAC_ADDR_FRAME_OFST + 1]
-        = frame[SOURCE_MAC_ADDR_FRAME_OFST + 1];
-    frame[SENDER_MAC_ADDR_FRAME_OFST + 2]
-        = frame[SOURCE_MAC_ADDR_FRAME_OFST + 2];
-    frame[SENDER_MAC_ADDR_FRAME_OFST + 3]
-        = frame[SOURCE_MAC_ADDR_FRAME_OFST + 3];
-    frame[SENDER_MAC_ADDR_FRAME_OFST + 4]
-        = frame[SOURCE_MAC_ADDR_FRAME_OFST + 4];
-    frame[SENDER_MAC_ADDR_FRAME_OFST + 5]
-        = frame[SOURCE_MAC_ADDR_FRAME_OFST + 5];
-
-    frame[TARGET_IP_ADDR_FRAME_OFST]     = frame[SENDER_IP_ADDR_FRAME_OFST];
-    frame[TARGET_IP_ADDR_FRAME_OFST + 1] = frame[SENDER_IP_ADDR_FRAME_OFST + 1];
-    frame[TARGET_IP_ADDR_FRAME_OFST + 2] = frame[SENDER_IP_ADDR_FRAME_OFST + 2];
-    frame[TARGET_IP_ADDR_FRAME_OFST + 3] = frame[SENDER_IP_ADDR_FRAME_OFST + 3];
-
-    frame[SENDER_IP_ADDR_FRAME_OFST]     = self->ip_addr[0];
-    frame[SENDER_IP_ADDR_FRAME_OFST + 1] = self->ip_addr[1];
-    frame[SENDER_IP_ADDR_FRAME_OFST + 2] = self->ip_addr[2];
-    frame[SENDER_IP_ADDR_FRAME_OFST + 3] = self->ip_addr[3];
-
+    memcpy(tx_frame + DEST_MAC_ADDR_FRAME_OFST, mdata->dest_mac_addr, 6);
+    memcpy(tx_frame + DEST_IP_ADDR_FRAME_OFST, mdata->dest_ip_addr, 4);
+    memcpy(tx_frame + SRC_MAC_ADDR_FRAME_OFST, self->mac_addr, 6);
+    memcpy(tx_frame + SRC_IP_ADDR_FRAME_OFST, self->ip_addr, 4);
     return 0;
 }
 
