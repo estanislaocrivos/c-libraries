8b1872154f1799180537e30cce2e4f1ae9a6a707
Estanislao Crivos <estanislaocrivos@gmail.com>
2026-07-02 22:34:44 -0300

    feat(icmp): refactor ICMP structures and functions for clarity and consistency

--- libraries/pico-net-stack/inc/icmp.h
+++ libraries/pico-net-stack/inc/icmp.h
@@ -1,4 +1,47 @@
 #ifndef ICMP_H
 #define ICMP_H
 
+#include <stdint.h>
+
+#define ICMP_HDR_SIZE     8
+#define ICMP_ECHO_REQUEST 8
+#define ICMP_ECHO_REPLY   0
+
+struct icmp
+{
+    int unused;
+};
+
+struct icmp_rx_metadata
+{
+    uint8_t        type;
+    uint8_t        code;
+    uint16_t       id;
+    uint16_t       seq_num;
+    const uint8_t* payload;
+    uint16_t       payload_size;
+};
+
+struct icmp_tx_metadata
+{
+    uint8_t        type;
+    uint8_t        code;
+    uint16_t       id;
+    uint16_t       seq_num;
+    const uint8_t* payload;
+    uint16_t       payload_size;
+};
+
+int8_t icmp_process_frame(
+    const struct icmp*       self,
+    const uint8_t*           rx_frame,
+    uint16_t                 rx_frame_size,
+    struct icmp_rx_metadata* mdata);
+
+int8_t icmp_build_frame(
+    const struct icmp*       self,
+    struct icmp_tx_metadata* mdata,
+    uint8_t*                 tx_frame,
+    uint16_t                 tx_frame_size);
+
 #endif
--- libraries/pico-net-stack/src/icmp.c
+++ libraries/pico-net-stack/src/icmp.c
@@ -1,39 +1,34 @@
 #include "../inc/icmp.h"
 
-#include <stdbool.h>
-#include <stdint.h>
-
-struct icmp
-{
-    bool nothing;
-};
-
-struct icmp_rx_metadata
-{
-    uint8_t  type;
-    uint8_t  code;
-    uint16_t checksum;
-    uint16_t id;
-    uint16_t seq_num;
-};
-
-struct icmp_tx_metadata
-{
-    uint8_t  type;
-    uint8_t  code;
-    uint16_t checksum;
-    uint16_t id;
-    uint16_t seq_num;
-};
+#include "../../inc/errno.h"
+#include "../inc/utils.h"
 
 int8_t icmp_process_frame(
     const struct icmp*       self,
     const uint8_t*           rx_frame,
     uint8_t                  rx_frame_size,
-    struct icmp_rx_metadata* mdata);
+    struct icmp_rx_metadata* mdata)
+{
+    if (compute_inet_checksum(rx_frame, rx_frame_size) != 0)
+    {
+        return -EINVAL;
+    }
+
+    mdata->type         = rx_frame[0];
+    mdata->code         = rx_frame[1];
+    mdata->id           = rx_frame[4];
+    mdata->seq_num      = rx_frame[6];
+    mdata->payload      = rx_frame + 8;
+    mdata->payload_size = rx_frame_size - 8;
+
+    return 0;
+}
 
 int8_t icmp_build_frame(
     const struct icmp*       self,
     struct icmp_tx_metadata* mdata,
     const uint8_t*           tx_frame,
-    uint8_t                  tx_frame_size);
+    uint8_t                  tx_frame_size)
+{
+    return 0;
+}
