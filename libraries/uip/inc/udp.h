4b71805701026819d8e960252f0949d782d11eb6
Estanislao Crivos <estanislaocrivos@gmail.com>
2026-07-10 11:07:19 -0300

    feat: add udp layer implementation

---
+++ libraries/uip/inc/udp.h
@@ -0,0 +1,34 @@
+#ifndef UDP_H
+#define UDP_H
+
+/* ========================================================================== */
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/* ========================================================================== */
+
+struct udp
+{
+    uint16_t lost_frames;
+};
+
+struct udp_rx_metadata
+{
+    uint16_t       src_port_num;
+    uint16_t       dest_port_num;
+    uint16_t       payload_size;
+    const uint8_t* payload;
+};
+
+/* ========================================================================== */
+
+int8_t udp_process_frame(
+    const struct udp*       self,
+    const uint8_t*          rx_frame,
+    uint16_t                rx_frame_size,
+    struct udp_rx_metadata* mdata);
+
+/* ========================================================================== */
+
+#endif /* UDP_H */
---
+++ libraries/uip/src/udp.c
@@ -0,0 +1,10 @@
+#include "../inc/udp.h"
+
+int8_t udp_process_frame(
+    const struct udp*       self,
+    const uint8_t*          rx_frame,
+    uint16_t                rx_frame_size,
+    struct udp_rx_metadata* mdata)
+{
+    return 0;
+}
