2e744e06f6b4f57ddd6a6261b777a172b9a70bf6
Estanislao Crivos <estanislaocrivos@gmail.com>
2026-07-10 19:17:34 -0300

    feat(udp): implement UDP checksum computation and frame building

--- libraries/uip/inc/udp.h
+++ libraries/uip/inc/udp.h
@@ -12,16 +12,25 @@
 
 struct udp
 {
-    uint16_t   lost_frames;
-    struct ip* ip;
+    uint16_t lost_frames;
 };
 
 struct udp_rx_metadata
 {
-    uint16_t       src_port_num;
-    uint16_t       dest_port_num;
-    uint16_t       payload_size;
-    const uint8_t* payload;
+    struct ip_rx_metadata* ip_mdata;
+    uint16_t               src_port_num;
+    uint16_t               dest_port_num;
+    const uint8_t*         payload;
+    uint16_t               payload_size;
+};
+
+struct udp_tx_metadata
+{
+    struct ip_tx_metadata* ip_mdata;
+    uint16_t               src_port_num;
+    uint16_t               dest_port_num;
+    uint8_t*               payload;
+    uint16_t               payload_size;
 };
 
 /* ========================================================================== */
@@ -31,6 +40,12 @@ int8_t udp_process_frame(
     const uint8_t*          rx_frame,
     uint16_t                rx_frame_size,
     struct udp_rx_metadata* mdata);
+
+int8_t udp_buid_frame(
+    struct udp*             self,
+    struct udp_rx_metadata* mdata,
+    uint8_t*                tx_frame,
+    uint16_t                tx_frame_size);
 
 /* ========================================================================== */
 
--- libraries/uip/src/udp.c
+++ libraries/uip/src/udp.c
@@ -4,6 +4,7 @@
 
 #include "../../inc/errno.h"
 #include "../inc/utils.h"
+#include "ip.h"
 
 #include <stddef.h>
 
@@ -22,6 +23,27 @@
 
 /* ========================================================================== */
 
+static uint16_t compute_udp_checksum(
+    const uint8_t* src_ip,
+    const uint8_t* dest_ip,
+    const uint8_t* rx_frame,
+    uint16_t       rx_frame_size)
+{
+    uint8_t udp_pseudo_header[4]
+        = {0,
+           IP_PLD_PROT_UDP_VAL,
+           rx_frame[UDP_LENGTH_FRAME_OFST],
+           rx_frame[UDP_LENGTH_FRAME_OFST + 1]};
+    struct slice frame_slice[]
+        = {{.base = src_ip, .len = 4},
+           {.base = dest_ip, .len = 4},
+           {.base = udp_pseudo_header, .len = sizeof(udp_pseudo_header)},
+           {.base = rx_frame, .len = rx_frame_size}};
+    return compute_inet_checksum(frame_slice, 4);
+}
+
+/* ========================================================================== */
+
 int8_t udp_process_frame(
     struct udp*             self,
     const uint8_t*          rx_frame,
@@ -33,17 +55,12 @@ int8_t udp_process_frame(
         return -EFAULT;
     }
 
-    uint8_t udp_pseudo_header[4]
-        = {0,
-           IP_PLD_PROT_UDP_VAL,
-           (uint8_t)(rx_frame[UDP_LENGTH_FRAME_OFST]),
-           (uint8_t)(rx_frame[UDP_LENGTH_FRAME_OFST + 1])};
-    struct slice frame_slice[]
-        = {{.base = self->ip->mdata->src_ip, .len = 4},
-           {.base = self->ip->mdata->dest_ip, .len = 4},
-           {.base = udp_pseudo_header, .len = sizeof(udp_pseudo_header)},
-           {.base = rx_frame, .len = rx_frame_size}};
-    if (compute_inet_checksum(frame_slice, 4) != 0)
+    if (compute_udp_checksum(
+            mdata->ip_mdata->src_ip,
+            mdata->ip_mdata->dest_ip,
+            rx_frame,
+            rx_frame_size)
+        != 0)
     {
         self->lost_frames += 1;
         return -EINVAL;
@@ -55,6 +72,17 @@ int8_t udp_process_frame(
                            | (uint16_t)(rx_frame[UDP_DEST_PORT_FRAME_OFST + 1]);
     mdata->payload      = rx_frame + UDP_PAYLOAD_FRAME_OFST;
     mdata->payload_size = rx_frame[UDP_LENGTH_FRAME_OFST] - UDP_HEADER_SIZE;
+    return 0;
+}
+
+/* ========================================================================== */
+
+int8_t udp_buid_frame(
+    struct udp*             self,
+    struct udp_rx_metadata* mdata,
+    uint8_t*                tx_frame,
+    uint16_t                tx_frame_size)
+{
     return 0;
 }
 
